d993ac4114ebb7e00126c7e47013f3ddbae345fd
Colin Meret <colinmeret@gmail.com>
2015-02-04 14:20:47 -0500

    added server.listen to start listening for incoming mail

--- app.js
+++ app.js
@@ -46,7 +46,9 @@ server.addListener('request', function(req, res) {
   });
 
 });
-
+var port = process.env.PORT || 3000;
+console.log(' [*] Listening on 0.0.0.0:' + port);
+server.listen(port, '0.0.0.0');
 
 //mailin.on('authorizeUser', funciton( connection, username, password, done) {
 //  if (username == "colin" && password == "mysecret") {
